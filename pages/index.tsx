1d7461d9fd75f2aff10e521a21d704ce342e1d4c
Нуртилек Кошкар <nurtylek98@gmail.com>
2020-12-01 18:17:22 +0600

    infinite scroll

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,40 +1,50 @@
-import {useCallback, useEffect, useRef, useState} from "react";
+import { useEffect, useRef, useState} from "react";
 import Image from "next/image";
 import {AppShell} from "@/components/app-shell/app-shell";
 import styles from '../styles/Home.module.css';
 import {defaultQueryFn} from "@/lib/api-hooks";
 
 export default function Home({photos}) {
-    const [, setSearchTerm] = useState('');
+    const [searchTerm, setSearchTerm] = useState('');
     const [searchPhotos, setSearchPhotos] = useState([]);
+    const [page, setPage] = useState(1);
+    const observer = useRef<IntersectionObserver>();
+    const [element, setElement] = useState(null);
 
-    const search = useCallback(async (searchTerm: string) => {
+    const search = async () => {
         try {
-            setSearchTerm(searchTerm);
-            const res = await defaultQueryFn(`${process.env.NEXT_PUBLIC_API_URL}search/photos?client_id=${process.env.NEXT_PUBLIC_ACCESS_KEY}&query=${searchTerm}`)
-            setSearchPhotos(res.results);
+            const res = await defaultQueryFn(`${process.env.NEXT_PUBLIC_API_URL}search/photos?client_id=${process.env.NEXT_PUBLIC_ACCESS_KEY}&query=${searchTerm}&page=${page}&per_page=12`)
+            setSearchPhotos(photos => {
+                // debug purposes
+                return [...photos, ...res.results]
+            });
         } catch (e) {
             console.log({e})
         }
-    }, []);
+    }
+
+    useEffect(() => {
+
+        if (searchTerm) {
+            search();
+            console.log(searchTerm)
+        }
+
+    }, [searchTerm, page]);
 
     useEffect(() => {
         setSearchPhotos(photos);
     }, []);
 
-    const observer = useRef<IntersectionObserver>();
-    const [element, setElement] = useState(null);
-
     useEffect(() => {
         observer.current = new IntersectionObserver(entries => {
             if (entries[0].isIntersecting) {
-                console.log(entries)
+                setPage(oldPage => oldPage + 1);
             }
-        }, {threshold: 1})
+        }, {threshold: 0.5, rootMargin: '200px 0px 0px 0px'})
     }, []);
 
     useEffect(() => {
-
         const currentElement = element;
         const currentObserver = observer.current;
 
@@ -50,10 +60,10 @@ export default function Home({photos}) {
     }, [element])
 
     return (
-        <AppShell title={'Create Next App'} search={searchTerm => search(searchTerm)}>
+        <AppShell title={'Create Next App'} search={searchTerm => setSearchTerm(searchTerm)}>
             {searchPhotos.length && (
-                <section className={styles.main__content}>
-                    <>
+                <>
+                    <section className={styles.main__content}>
                         {searchPhotos.map(photo => {
                             return (
                                 <figure key={photo.id + photo}>
@@ -66,9 +76,9 @@ export default function Home({photos}) {
                                 </figure>
                             )
                         })}
-                        <div ref={setElement}><button>Load More</button></div>
-                    </>
-                </section>
+                    </section>
+                    <div ref={setElement}><button>Load More</button></div>
+                </>
             )}
         </AppShell>
     )

9639236e54a2c59dc7e5bb514b6c4480d2857725
Нуртилек Кошкар <nurtylek98@gmail.com>
2020-12-01 17:38:46 +0600

    add intersection observer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,63 +1,73 @@
-import {useCallback, useEffect, useState} from "react";
+import {useCallback, useEffect, useRef, useState} from "react";
 import Image from "next/image";
 import {AppShell} from "@/components/app-shell/app-shell";
 import styles from '../styles/Home.module.css';
 import {defaultQueryFn} from "@/lib/api-hooks";
 
 export default function Home({photos}) {
-    const [searchTerm, setSearchTerm] = useState('');
+    const [, setSearchTerm] = useState('');
     const [searchPhotos, setSearchPhotos] = useState([]);
-    const [page, setPage] = useState(1);
-    const [totalPages, setTotalPages] = useState(1);
 
     const search = useCallback(async (searchTerm: string) => {
         try {
             setSearchTerm(searchTerm);
             const res = await defaultQueryFn(`${process.env.NEXT_PUBLIC_API_URL}search/photos?client_id=${process.env.NEXT_PUBLIC_ACCESS_KEY}&query=${searchTerm}`)
             setSearchPhotos(res.results);
-            setTotalPages(res.total_pages);
-            console.log(totalPages)
         } catch (e) {
             console.log({e})
         }
     }, []);
 
-    const nextPage = () => {
-        setPage(oldPage => oldPage + 1);
-    }
-
-    useEffect(() => {
-        async function nextPage() {
-            const res = await defaultQueryFn(`${process.env.NEXT_PUBLIC_API_URL}search/photos?client_id=${process.env.NEXT_PUBLIC_ACCESS_KEY}&query=${searchTerm}&page=${page}`)
-            setSearchPhotos(oldPhotos => [...oldPhotos, ...res.results]);
-        }
-
-        nextPage();
-    }, [page])
-
     useEffect(() => {
         setSearchPhotos(photos);
-    }, [])
+    }, []);
+
+    const observer = useRef<IntersectionObserver>();
+    const [element, setElement] = useState(null);
+
+    useEffect(() => {
+        observer.current = new IntersectionObserver(entries => {
+            if (entries[0].isIntersecting) {
+                console.log(entries)
+            }
+        }, {threshold: 1})
+    }, []);
+
+    useEffect(() => {
+
+        const currentElement = element;
+        const currentObserver = observer.current;
+
+        if (currentElement) {
+            currentObserver.observe(currentElement);
+        }
+
+        return () => {
+            if (currentElement) {
+                currentObserver.unobserve(currentElement);
+            }
+        }
+    }, [element])
 
     return (
         <AppShell title={'Create Next App'} search={searchTerm => search(searchTerm)}>
             {searchPhotos.length && (
                 <section className={styles.main__content}>
-                    {searchPhotos.map(photo => {
-                        return (
-                            <figure key={photo.id + photo}>
-                                <Image
-                                    src={photo.urls.regular}
-                                    alt={photo.alt_description}
-                                    width={photo.width}
-                                    height={photo.height}
-                                />
-                            </figure>
-                        )
-                    })}
-                    {totalPages > 1 && (
-                        <button onClick={() => nextPage()}>Next</button>
-                    )}
+                    <>
+                        {searchPhotos.map(photo => {
+                            return (
+                                <figure key={photo.id + photo}>
+                                    <Image
+                                        src={photo.urls.regular}
+                                        alt={photo.alt_description}
+                                        width={photo.width}
+                                        height={photo.height}
+                                    />
+                                </figure>
+                            )
+                        })}
+                        <div ref={setElement}><button>Load More</button></div>
+                    </>
                 </section>
             )}
         </AppShell>
